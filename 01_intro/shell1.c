13552d137a0d3268de4f7e1e9eb76daf2acbb557
Alex Lam <crudalex@gmail.com>
2018-09-21 10:49:42 +0800

    02_standard
    Signed-off-by: Alex Lam <crudalex@gmail.com>

--- 01_intro/shell1.c
+++ 01_intro/shell1.c
@@ -1,42 +1,37 @@
 #include "apue.h"
 #include <sys/wait.h>
 
-int main (void)
-{
-  char buf[1000];
-  pid_t pid;
-  int status;
+int main(void) {
+    char buf[1000];
+    pid_t pid;
+    int status;
 
-  printf ("%% ");
+    printf("%% ");
 
-  while (fgets (buf, 1000, stdin) != NULL)
-  {
-    if (buf[strlen (buf) - 1] == '\n')
-    {
-      buf[strlen (buf) - 1] = 0;
+    while (fgets(buf, 1000, stdin) != NULL) {
+        if (buf[strlen(buf) - 1] == '\n') {
+            buf[strlen(buf) - 1] = 0;
 
+        }
+
+        if ((pid = fork()) < 0) {
+            err_sys("fork error");
+
+        }
+
+        // child
+        if (pid == 0) {
+            execlp(buf, buf, (char *) 0);
+            err_ret("couldn't execute: %s", buf);
+            exit(127);
+        }
+
+        // parent
+        // if ((pid = waitpid (pid, &status, 0)) < 0)
+        if ((waitpid(pid, &status, 0)) < 0) {
+            err_sys("waitpid error");
+        }
+        printf("%% ");
     }
-
-    if ((pid = fork ()) < 0)
-    {
-      err_sys ("fork error");
-
-    }
-
-    // child
-    if (pid == 0)
-    {
-      execlp (buf, buf, (char *) 0);
-      err_ret ("couldn't execute: %s", buf);
-      exit (127);
-    }
-
-    // parent
-    if ((pid = waitpid (pid, &status, 0)) < 0)
-    {
-      err_sys ("waitpid error");
-    }
-    printf ("%% ");
-  }
-  exit (0);
+    exit(0);
 }
--- 01_intro/shell2.c
+++ 01_intro/shell2.c
@@ -3,31 +3,30 @@
 
 static void sig_int(int);        /* our signal-catching function */
 
-int main(void)
-{
+int main(void) {
     char buf[MAXLINE];    /* from apue.h */
     pid_t pid;
     int status;
 
-    if (signal(SIGINT, sig_int)==SIG_ERR)
+    if (signal(SIGINT, sig_int) == SIG_ERR)
         err_sys("signal error");
 
     printf("%% ");    /* print prompt (printf requires %% to print %) */
-    while (fgets(buf, MAXLINE, stdin)!=NULL) {
-        if (buf[strlen(buf)-1]=='\n')
-            buf[strlen(buf)-1] = 0; /* replace newline with null */
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        if (buf[strlen(buf) - 1] == '\n')
+            buf[strlen(buf) - 1] = 0; /* replace newline with null */
 
-        if ((pid = fork())<0) {
+        if ((pid = fork()) < 0) {
             err_sys("fork error");
-        }
-        else if (pid==0) {        /* child */
-            execlp(buf, buf, (char*) 0);
+        } else if (pid == 0) {        /* child */
+            execlp(buf, buf, (char *) 0);
             err_ret("couldn't execute: %s", buf);
             exit(127);
         }
 
         /* parent */
-        if ((pid = waitpid(pid, &status, 0))<0)
+        // if ((pid = waitpid(pid, &status, 0))<0)
+        if ((waitpid(pid, &status, 0)) < 0)
             err_sys("waitpid error");
         printf("%% ");
     }
@@ -35,7 +34,6 @@ int main(void)
 }
 
 void
-sig_int(int signo)
-{
-    printf("interrupt\n%% ");
+sig_int(int signo) {
+    printf("interrupt: %d\n%% ", signo);
 }
