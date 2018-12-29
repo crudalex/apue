8efc96dc3b0800ec91b67803e68d815ddfb54e11
Alex Lam <crudalex@gmail.com>
2018-12-29 15:22:28 +0800

    Initial commit
    Signed-off-by: atwlam <crudalex@gmail.com>
    Signed-off-by: Alex Lam <crudalex@gmail.com>

---
+++ lsp/9_MemoryManagement/mem_align.c
@@ -0,0 +1,12 @@
+//
+// Created by atwlam on 12/30/2018.
+//
+
+int main(void) {
+    char greeting[] = "Ahoy Matey";
+    char *c;
+    c = &greeting[1];
+    unsigned long badnews = *(unsigned long *) c;
+}
+
+
